bffdd6904b6f14676b2bc3df7cf681b74777944b
lootag <luigi.taglialatela1996@gmail.com>
2020-11-03 16:06:45 +0100

    iterating over images

--- RetinanetTutorial/testDetector.py
+++ RetinanetTutorial/testDetector.py
@@ -25,8 +25,8 @@ from PIL import Image
 import tensorflow as tf
 
 model_path = '../Models/logos_inference.h5'
-image_path = '../adidasTest.jpg'
-image_output_path = '../adidasDetected.jpg'
+image_paths = ['../Input/' + name for name in os.listdir('../Input')]
+image_output_paths = ['../Output/' + name for name in os.listdir('../Input')]
 confidence_cutoff = 0.5 #Detections below this confidence will be ignored
 
 def get_session():
@@ -43,90 +43,90 @@ def get_session():
 # adjust this to point to your downloaded/trained model
 # models can be downloaded here: https://github.com/fizyr/keras-retinanet/releases
 #model_path = os.path.join('..', 'snapshots', 'resnet50_coco_best_v2.1.0.h5')
+for imageIndex in range(len(image_paths)):
+    print("Loading image from {}".format(image_paths[imageIndex]))
+    image = np.asarray(Image.open(image_paths[imageIndex]).convert('RGB'))
+    image = image[:, :, ::-1].copy()
 
-print("Loading image from {}".format(image_path))
-image = np.asarray(Image.open(image_path).convert('RGB'))
-image = image[:, :, ::-1].copy()
+    # load retinanet model
+    print("Loading Model: {}".format(model_path))
+    model = models.load_model(model_path, backbone_name='resnet50')
 
-# load retinanet model
-print("Loading Model: {}".format(model_path))
-model = models.load_model(model_path, backbone_name='resnet50')
+    #Check that it's been converted to an inference model
+    try:
+        model = models.convert_model(model)
+    except:
+        print("Model is likely already an inference model")
 
-#Check that it's been converted to an inference model
-try:
-    model = models.convert_model(model)
-except:
-    print("Model is likely already an inference model")
+    # load label to names mapping for visualization purposes
+    labels_to_names = {
+            0: 'adidas_1', 
+            1: 'adidas_2',
+            3: 'tommy_ext',
+            4: 'tommy_flag',
+            5: 'lv_text',
+            6: 'lvc',
+            7: 'hermes',
+            8: 'hermesl',
+            9: 'hermes_h',
+            10: 'gucci_o',
+            11: 'gucci',
+            12: 'gucci_text',
+            13: 'fendi',
+            14: 'fendi_text',
+            15: 'mk',
+            16: 'mk_text',
+            17: 'rf',
+            18: 'rf_text',
+            19: 'ck',
+            20: 'ck_text',
+            21: 'ck_old' ,
+            22: 'ck_performance',
+            23: 'chanel',
+            24: 'chanel_text',
+            25: 'dior',
+            26: 'jadior',
+            27: 'lv'
+    }
 
-# load label to names mapping for visualization purposes
-labels_to_names = {
-        0: 'adidas_1', 
-        1: 'adidas_2',
-        3: 'tommy_ext',
-        4: 'tommy_flag',
-        5: 'lv_text',
-        6: 'lvc',
-        7: 'hermes',
-        8: 'hermesl',
-        9: 'hermes_h',
-        10: 'gucci_o',
-        11: 'gucci',
-        12: 'gucci_text',
-        13: 'fendi',
-        14: 'fendi_text',
-        15: 'mk',
-        16: 'mk_text',
-        17: 'rf',
-        18: 'rf_text',
-        19: 'ck',
-        20: 'ck_text',
-        21: 'ck_old' ,
-        22: 'ck_performance',
-        23: 'chanel',
-        24: 'chanel_text',
-        25: 'dior',
-        26: 'jadior',
-        27: 'lv'
-}
+    # copy to draw on
+    draw = image.copy()
+    draw = cv2.cvtColor(draw, cv2.COLOR_BGR2RGB)
 
-# copy to draw on
-draw = image.copy()
-draw = cv2.cvtColor(draw, cv2.COLOR_BGR2RGB)
+    # Image formatting specific to Retinanet
+    image = preprocess_image(image)
+    image, scale = resize_image(image)
 
-# Image formatting specific to Retinanet
-image = preprocess_image(image)
-image, scale = resize_image(image)
+    # Run the inference
+    start = time.time()
 
-# Run the inference
-start = time.time()
+    boxes, scores, labels = model.predict_on_batch(np.expand_dims(image, axis=0))
+    print("processing time: ", time.time() - start)
 
-boxes, scores, labels = model.predict_on_batch(np.expand_dims(image, axis=0))
-print("processing time: ", time.time() - start)
+    # correct for image scale
+    boxes /= scale
 
-# correct for image scale
-boxes /= scale
+    # visualize detections
+    for box, score, label in zip(boxes[0], scores[0], labels[0]):
+        # scores are sorted so we can break
+        if score < confidence_cutoff:
+            break
 
-# visualize detections
-for box, score, label in zip(boxes[0], scores[0], labels[0]):
-    # scores are sorted so we can break
-    if score < confidence_cutoff:
-        break
+        #Add boxes and captions
+        color = (255, 255, 255)
+        thickness = 2
+        b = np.array(box).astype(int)
+        cv2.rectangle(draw, (b[0], b[1]), (b[2], b[3]), color, thickness, cv2.LINE_AA)
 
-    #Add boxes and captions
-    color = (255, 255, 255)
-    thickness = 2
-    b = np.array(box).astype(int)
-    cv2.rectangle(draw, (b[0], b[1]), (b[2], b[3]), color, thickness, cv2.LINE_AA)
+        if(label > len(labels_to_names)):
+            print("WARNING: Got unknown label, using 'detection' instead")
+            caption = "Detection {:.3f}".format(score)
+        else:
+            caption = "{} {:.3f}".format(labels_to_names[label], score)
 
-    if(label > len(labels_to_names)):
-        print("WARNING: Got unknown label, using 'detection' instead")
-        caption = "Detection {:.3f}".format(score)
-    else:
-        caption = "{} {:.3f}".format(labels_to_names[label], score)
+        cv2.putText(draw, caption, (b[0], b[1] - 10), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 2)
+        cv2.putText(draw, caption, (b[0], b[1] - 10), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
 
-    cv2.putText(draw, caption, (b[0], b[1] - 10), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 2)
-    cv2.putText(draw, caption, (b[0], b[1] - 10), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
-
-#Write out image
-draw = Image.fromarray(draw)
-draw.save(image_output_path)
+    #Write out image
+    draw = Image.fromarray(draw)
+    draw.save(image_output_paths[imageIndex])
